68670d8011d7d2b78c4ef8992964a14f1b3a573e
tangdaoyuan <1197633750@qq.com>
2022-06-21 11:17:04 +0800

    feat: add commands

--- extension/src/command.ts
+++ extension/src/command.ts
@@ -9,37 +9,62 @@ export default function registerCommands(context: ExtensionContext) {
 
 function registerNowTimeCommand(context: ExtensionContext) {
   const disposable = commands.registerCommand('author', () => {
-    // eslint-disable-next-line no-console
-    console.log(window.activeTextEditor?.document.getText())
     window.showWarningMessage('Extension Author: Tedy (tangdaoyuan)')
   })
   context.subscriptions.push(disposable)
 }
 
+const commandWithOptions = {
+  'cc.default': {
+    alpha: false,
+    color: false,
+  },
+  'cc.all': {
+    alpha: true,
+    color: true,
+  },
+  'cc.color': {
+    alpha: false,
+    color: true,
+  },
+  'cc.alpha': {
+    alpha: true,
+    color: false,
+  },
+}
+
 function registerColorPickerCommand(context: ExtensionContext) {
-  const disposable = commands.registerCommand('cc.default', async() => {
-    const activeDoc = window.activeTextEditor?.document
-    try {
-      if (!activeDoc?.fileName.endsWith('.json')) {
-        window.showErrorMessage('Only support json file!')
-        return
+  for (const command in commandWithOptions) {
+    const disposable = commands.registerCommand(command, async() => {
+      const activeDoc = window.activeTextEditor?.document
+      try {
+        if (!activeDoc?.fileName.endsWith('.json')) {
+          window.showErrorMessage('Only support json file!')
+          return
+        }
+        await openColorsDoc(
+          activeDoc.getText(),
+          commandWithOptions[command as keyof typeof commandWithOptions],
+        )
       }
+      catch (error: unknown) {
+        window.showErrorMessage((error as Object).toString())
+      }
+    })
+    context.subscriptions.push(disposable)
+  }
+}
 
-      const content = parseColors(JSON.parse(activeDoc.getText()))
+async function openColorsDoc(text: string, options: { color: boolean; alpha: boolean }) {
+  const content = parseColors(JSON.parse(text), options.alpha, options.color)
 
-      const document = await workspace.openTextDocument({
-        content: JSON.stringify(content, null, 2),
-        language: 'json',
-      })
-      if (document)
-        window.showTextDocument(document)
-
-      else
-        window.showInformationMessage('Error!')
-    }
-    catch (error: unknown) {
-      window.showErrorMessage((error as Object).toString())
-    }
+  const document = await workspace.openTextDocument({
+    content: JSON.stringify(content, null, 2),
+    language: 'json',
   })
-  context.subscriptions.push(disposable)
+  if (document)
+    window.showTextDocument(document)
+
+  else
+    window.showInformationMessage('Error!')
 }
